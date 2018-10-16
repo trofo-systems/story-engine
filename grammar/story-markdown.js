1eba915fbf3fabcddf8de3cc5c969cb962eab952
Andre Rosot <alrosot@gmail.com>
2018-10-16 22:37:33 +1100

    A very strict (but working grammar)

--- grammar/story-markdown.js
+++ grammar/story-markdown.js
@@ -6,7 +6,14 @@ function id(x) { return x[0]; }
 var itemAt = function (a) { return function (d) {return d[a]; } };
 var buildMap = function(a,b) {return function(d){ actions = {}; actions[d[a]] = d[b]; return actions; }};
 var emptyStr = function (d) { return ""; };
-var buildState = function(name, text, prompt, final){state = {name: name[0], text:text};if(final === true) {state.final = true;} return state;};
+var buildState = function(name, text, prompt, final, nextAction){
+state = {name: name[0], text:text};
+
+if(final === true) {state.final = true;}
+if(nextAction) {state.nextAction = nextAction}
+ return state;
+};
+
 var mergeMap = function(map, element){ return function(d){ for (var property in d[element]) {
                                                                if (d[element].hasOwnProperty(property)) {
                                                                    d[map][property] = d[element][property];
@@ -47,10 +54,18 @@ var grammar = {
     {"name": "non_actionable_state$ebnf$1", "symbols": ["non_actionable_state$ebnf$1", "state_name"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "non_actionable_state$ebnf$2", "symbols": []},
     {"name": "non_actionable_state$ebnf$2", "symbols": ["non_actionable_state$ebnf$2", "newline"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "non_actionable_state$ebnf$3", "symbols": []},
-    {"name": "non_actionable_state$ebnf$3$string$1", "symbols": [{"literal":"#"}, {"literal":"#"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "non_actionable_state$ebnf$3", "symbols": ["non_actionable_state$ebnf$3", "non_actionable_state$ebnf$3$string$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "non_actionable_state", "symbols": ["non_actionable_state$ebnf$1", "non_empty_string", "non_actionable_state$ebnf$2", "non_actionable_state$ebnf$3"], "postprocess": function(d){return buildState(d[0], d[1],null,d[3].length > 0)}},
+    {"name": "non_actionable_state$ebnf$3$subexpression$1$string$1", "symbols": [{"literal":"#"}, {"literal":"#"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "non_actionable_state$ebnf$3$subexpression$1", "symbols": ["non_actionable_state$ebnf$3$subexpression$1$string$1"]},
+    {"name": "non_actionable_state$ebnf$3$subexpression$1", "symbols": ["next_action"]},
+    {"name": "non_actionable_state$ebnf$3", "symbols": ["non_actionable_state$ebnf$3$subexpression$1"], "postprocess": id},
+    {"name": "non_actionable_state$ebnf$3", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "non_actionable_state", "symbols": ["non_actionable_state$ebnf$1", "non_empty_string", "non_actionable_state$ebnf$2", "non_actionable_state$ebnf$3"], "postprocess":  function(d){
+          var final = (d[3] && d[3].length > 0 && d[3][0] == "##");
+          if(!final){
+            var nextAction = d[3]? d[3][0]:null;
+          }
+          return buildState(d[0], d[1],null,final, nextAction);
+        } },
     {"name": "prompt$ebnf$1", "symbols": []},
     {"name": "prompt$ebnf$1", "symbols": ["prompt$ebnf$1", /[\s]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "prompt", "symbols": [{"literal":">"}, "prompt$ebnf$1", "non_empty_string"], "postprocess": itemAt(2)},
@@ -65,6 +80,8 @@ var grammar = {
     {"name": "action", "symbols": [{"literal":":"}, "non_empty_string", "action$string$1", "non_empty_string"], "postprocess": buildMap(1,3)},
     {"name": "default_action$string$1", "symbols": [{"literal":"-"}, {"literal":"-"}, {"literal":">"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "default_action", "symbols": ["default_action$string$1", "non_empty_string"], "postprocess": itemAt(1)},
+    {"name": "next_action$string$1", "symbols": [{"literal":"-"}, {"literal":"-"}, {"literal":"-"}, {"literal":">"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "next_action", "symbols": ["next_action$string$1", "non_empty_string"], "postprocess": itemAt(1)},
     {"name": "non_empty_string$ebnf$1", "symbols": []},
     {"name": "non_empty_string$ebnf$1", "symbols": ["non_empty_string$ebnf$1", /[^\n\r]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "non_empty_string", "symbols": [/[A-Za-z]/, "non_empty_string$ebnf$1"], "postprocess": function(d){return d[0]+ d[1].join("") }}
--- grammar/story-markdown.ne
+++ grammar/story-markdown.ne
@@ -2,7 +2,14 @@
 var itemAt = function (a) { return function (d) {return d[a]; } };
 var buildMap = function(a,b) {return function(d){ actions = {}; actions[d[a]] = d[b]; return actions; }};
 var emptyStr = function (d) { return ""; };
-var buildState = function(name, text, prompt, final){state = {name: name[0], text:text};if(final === true) {state.final = true;} return state;};
+var buildState = function(name, text, prompt, final, nextAction){
+state = {name: name[0], text:text};
+
+if(final === true) {state.final = true;}
+if(nextAction) {state.nextAction = nextAction}
+ return state;
+};
+
 var mergeMap = function(map, element){ return function(d){ for (var property in d[element]) {
                                                                if (d[element].hasOwnProperty(property)) {
                                                                    d[map][property] = d[element][property];
@@ -35,7 +42,13 @@ state -> non_actionable_state {% itemAt(0) %}
 
 actionable_state -> non_actionable_state prompt:* newline actions newline default_action:? {% actionableState %}
 
-non_actionable_state -> state_name:* non_empty_string newline:* "##":* {% function(d){return buildState(d[0], d[1],null,d[3].length > 0)} %}
+non_actionable_state -> state_name:* non_empty_string newline:* ("##" | next_action ):? {% function(d){
+  var final = (d[3] && d[3].length > 0 && d[3][0] == "##");
+  if(!final){
+    var nextAction = d[3]? d[3][0]:null;
+  }
+  return buildState(d[0], d[1],null,final, nextAction);
+} %}
 
 prompt ->  ">" [\s]:*  non_empty_string {% itemAt(2) %}
 
@@ -49,5 +62,7 @@ actions -> action {% itemAt(0) %}
 action ->  ":" non_empty_string  " -> " non_empty_string {% buildMap(1,3) %}
 
 default_action -> "-->" non_empty_string {% itemAt(1) %}
+
+next_action-> "--->" non_empty_string {% itemAt(1) %}
 
 non_empty_string ->  [A-Za-z] [^\n\r]:* {% function(d){return d[0]+ d[1].join("") } %}
